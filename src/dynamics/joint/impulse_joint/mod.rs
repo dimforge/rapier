0bb0e412e62807c487594f35673fbec3ea02a62f
Sébastien Crozet <developer@crozet.re>
2022-01-25 22:58:18 +0100

    Rename JointHandle -> ImpulseJointHandle

--- src/dynamics/joint/impulse_joint/mod.rs
+++ src/dynamics/joint/impulse_joint/mod.rs
@@ -1,5 +1,5 @@
 pub use self::impulse_joint::ImpulseJoint;
-pub use self::impulse_joint_set::{ImpulseJointSet, JointHandle};
+pub use self::impulse_joint_set::{ImpulseJointHandle, ImpulseJointSet};
 pub(crate) use self::impulse_joint_set::{JointGraphEdge, JointIndex};
 
 mod impulse_joint;
