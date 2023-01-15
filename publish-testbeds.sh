155fb9b1c08a2c86fb36462d953790befe965164
Sébastien Crozet <developer@crozet.re>
2023-01-15 16:37:15 +0100

    Use gsed in testbed publish script

--- publish-testbeds.sh
+++ publish-testbeds.sh
@@ -10,16 +10,16 @@ cp -r crates "$tmp"/.
 cp -r LICENSE README.md "$tmp"/.
 
 ### Publish the 2D version.
-sed 's#\.\./\.\./src#src#g' crates/rapier_testbed2d/Cargo.toml > "$tmp"/Cargo.toml
-sed -i 's#\.\./rapier#./crates/rapier#g' "$tmp"/Cargo.toml
+gsed 's#\.\./\.\./src#src#g' crates/rapier_testbed2d/Cargo.toml > "$tmp"/Cargo.toml
+gsed -i 's#\.\./rapier#./crates/rapier#g' "$tmp"/Cargo.toml
 currdir=$(pwd)
 cd "$tmp" && cargo publish
 cd "$currdir" || exit
 
 
 ### Publish the 3D version.
-sed 's#\.\./\.\./src#src#g' crates/rapier_testbed3d/Cargo.toml > "$tmp"/Cargo.toml
-sed -i 's#\.\./rapier#./crates/rapier#g' "$tmp"/Cargo.toml
+gsed 's#\.\./\.\./src#src#g' crates/rapier_testbed3d/Cargo.toml > "$tmp"/Cargo.toml
+gsed -i 's#\.\./rapier#./crates/rapier#g' "$tmp"/Cargo.toml
 cp -r LICENSE README.md "$tmp"/.
 cd "$tmp" && cargo publish
 
