4fcbd53bfe7c1d624e939a72fa25b44ebf67eb98
Jan Nils Ferner <contact@jnferner.com>
2023-01-26 20:41:48 +0100

    Refactor variable

--- src/control/character_controller.rs
+++ src/control/character_controller.rs
@@ -736,5 +736,6 @@ impl KinematicCharacterController {
 }
 
 fn subtract_hit(translation: Vector<Real>, hit: &TOI, offset: Real) -> Vector<Real> {
-    translation - *hit.normal1 * ((translation).dot(&hit.normal1) * (1.0 + offset)).min(0.0)
+    let hit_normal = (translation.dot(&hit.normal1) * (1.0 + offset)).min(0.0);
+    translation - *hit.normal1 * hit_normal
 }
