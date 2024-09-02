9dc8b2e67b877567654617d0048c1df728a45bbd
Thierry Berger <contact@thierryberger.com>
2024-09-02 14:36:44 +0200

    publish scripts better errors, abort if a publish fails, with an exit code.

--- publish-all.sh
+++ publish-all.sh
@@ -2,13 +2,13 @@
 
 if [[ "$PUBLISH_MODE" == 1 ]]
 then
-    ./scripts/publish-rapier.sh
-    ./scripts/publish-testbeds.sh
+    ./scripts/publish-rapier.sh &&
+    ./scripts/publish-testbeds.sh &&
     ./scripts/publish-extra-formats.sh
 else
     echo "Running in dry mode, re-run with \`PUBLISH_MODE=1 publish-all.sh\` to actually publish."
 
-    DRY_RUN="--dry-run" ./scripts/publish-rapier.sh
-    DRY_RUN="--dry-run" ./scripts/publish-testbeds.sh
+    DRY_RUN="--dry-run" ./scripts/publish-rapier.sh &&
+    DRY_RUN="--dry-run" ./scripts/publish-testbeds.sh &&
     DRY_RUN="--dry-run" ./scripts/publish-extra-formats.sh
 fi
--- scripts/publish-extra-formats.sh
+++ scripts/publish-extra-formats.sh
@@ -3,9 +3,9 @@
 currdir=$(pwd)
 
 ### Publish rapier3d-stl.
-cd "crates/rapier3d-stl" && cargo publish $DRY_RUN
-cd "$currdir" || exit
+cd "crates/rapier3d-stl" && cargo publish $DRY_RUN || exit 1
+cd "$currdir" || exit 2
 
 ### Publish rapier3d-urdf.
-cd "crates/rapier3d-urdf" && cargo publish $DRY_RUN
-cd "$currdir" || exit
+cd "crates/rapier3d-urdf" && cargo publish $DRY_RUN || exit 1
+cd "$currdir" || exit 2
