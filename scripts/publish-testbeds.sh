98f9199abc3044779627b859b14f84fff3a97623
Thierry Berger <contact@thierryberger.com>
2024-09-03 21:48:10 +0200

    chore: add publish script for urdf and stl + unify all releases by de… (#727)
    * chore: add publish script for urdf and stl + unify all releases by default
    
    * better cross platform publish support
    
    * publish in dry run within ci
    
    * publish scripts better errors, abort if a publish fails, with an exit code.
    
    * chore(rapier_urdf): fix warnings
    
    * chore(rapier-urdf): typo fix
    
    ---------
    
    Co-authored-by: Sébastien Crozet <developer@crozet.re>

---
+++ scripts/publish-testbeds.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+gsed -v >> /dev/null
+if [ $? == 0 ]; then
+    gsed=gsed
+else
+    # Hopefully installed sed is the gnu one.
+    gsed=sed
+fi
+
+tmp=$(mktemp -d)
+
+echo "$tmp"
+
+cp -r src "$tmp"/.
+cp -r src_testbed "$tmp"/.
+cp -r crates "$tmp"/.
+cp -r LICENSE README.md "$tmp"/.
+
+### Publish the 2D version.
+$gsed 's#\.\./\.\./src#src#g' crates/rapier_testbed2d/Cargo.toml > "$tmp"/Cargo.toml
+$gsed -i 's#\.\./rapier#./crates/rapier#g' "$tmp"/Cargo.toml
+currdir=$(pwd)
+cd "$tmp" && cargo publish $DRY_RUN
+cd "$currdir" || exit
+
+
+### Publish the 3D version.
+$gsed 's#\.\./\.\./src#src#g' crates/rapier_testbed3d/Cargo.toml > "$tmp"/Cargo.toml
+$gsed -i 's#\.\./rapier#./crates/rapier#g' "$tmp"/Cargo.toml
+cp -r LICENSE README.md "$tmp"/.
+cd "$tmp" && cargo publish $DRY_RUN
+
+rm -rf "$tmp"
