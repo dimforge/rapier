0312820a69e4897908f3ace901a31b0a4deb0b87
Thierry Berger <contact@thierryberger.com>
2024-11-04 17:52:07 +0100

    comment out meshloader publish script

--- scripts/publish-extra-formats.sh
+++ scripts/publish-extra-formats.sh
@@ -3,8 +3,9 @@
 currdir=$(pwd)
 
 ### Publish rapier3d-meshloader.
-cd "crates/rapier3d-meshloader" && cargo publish $DRY_RUN || exit 1
-cd "$currdir" || exit 2
+# FIXME: This is commented while we have not created the corresponding crate on crates.io
+# cd "crates/rapier3d-meshloader" && cargo publish $DRY_RUN || exit 1
+# cd "$currdir" || exit 2
 
 ### Publish rapier3d-urdf.
 cd "crates/rapier3d-urdf" && cargo publish $DRY_RUN || exit 1
