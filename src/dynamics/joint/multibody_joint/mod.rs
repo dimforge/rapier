11904cc4b937619173adbfa14b9facdabaf62057
Thierry Berger <contact@thierryberger.com>
2025-07-17 10:33:11 +0200

    simpler test because a vec of 1 doesn't need random ordering

--- src/dynamics/joint/multibody_joint/mod.rs
+++ src/dynamics/joint/multibody_joint/mod.rs
@@ -29,37 +29,74 @@ mod test {
 
     #[test]
     fn multibody_joint_remove_and_step() {
-        let mut rnd = oorandom::Rand32::new(1234);
+        let mut bodies = RigidBodySet::new();
+        let mut multibody_joints = MultibodyJointSet::new();
+        let mut colliders = ColliderSet::new();
+        let mut impulse_joints = ImpulseJointSet::new();
+        let mut islands = IslandManager::new();
 
-        for k in 0..10 {
-            let mut bodies = RigidBodySet::new();
-            let mut multibody_joints = MultibodyJointSet::new();
-            let mut colliders = ColliderSet::new();
-            let mut impulse_joints = ImpulseJointSet::new();
-            let mut islands = IslandManager::new();
+        let mut pipeline = PhysicsPipeline::new();
+        let mut bf = BroadPhaseMultiSap::new();
+        let mut nf = NarrowPhase::new();
 
-            let mut pipeline = PhysicsPipeline::new();
-            let mut bf = BroadPhaseMultiSap::new();
-            let mut nf = NarrowPhase::new();
+        let num_links = 2;
+        let mut handles = vec![];
 
-            let num_links = 2;
-            let mut handles = vec![];
+        for _ in 0..num_links {
+            use crate::prelude::RigidBodyBuilder;
 
-            for _ in 0..num_links {
-                use crate::prelude::RigidBodyBuilder;
+            handles.push(bodies.insert(RigidBodyBuilder::dynamic()));
+        }
 
-                handles.push(bodies.insert(RigidBodyBuilder::dynamic()));
-            }
+        #[cfg(feature = "dim2")]
+        let joint = RevoluteJoint::new();
+        #[cfg(feature = "dim3")]
+        let joint = RevoluteJoint::new(na::Vector::x_axis());
 
-            #[cfg(feature = "dim2")]
-            let joint = RevoluteJoint::new();
-            #[cfg(feature = "dim3")]
-            let joint = RevoluteJoint::new(na::Vector::x_axis());
+        for i in 0..num_links - 1 {
+            multibody_joints
+                .insert(handles[i], handles[i + 1], joint, true)
+                .unwrap();
+        }
+        pipeline.step(
+            &Vector::zeros(),
+            &IntegrationParameters::default(),
+            &mut islands,
+            &mut bf,
+            &mut nf,
+            &mut bodies,
+            &mut colliders,
+            &mut impulse_joints,
+            &mut multibody_joints,
+            &mut CCDSolver::new(),
+            None,
+            &(),
+            &(),
+        );
 
-            for i in 0..num_links - 1 {
-                multibody_joints
-                    .insert(handles[i], handles[i + 1], joint, true)
-                    .unwrap();
+        for handle in handles.clone().iter() {
+            bodies.remove(
+                *handle,
+                &mut islands,
+                &mut colliders,
+                &mut impulse_joints,
+                &mut multibody_joints,
+                true,
+            );
+            // Remove from our handles the one we just removed.
+            handles.retain(|value| value != handle);
+            // All handles left should be correctly set up.
+            for handle in handles.iter() {
+                // Note debug #847: rigid_body_link should return none when attempting to remove the last (second) body.
+                if let Some(link) = multibody_joints.rigid_body_link(*handle).copied() {
+                    if multibody_joints
+                        .get_multibody_mut_internal(link.multibody)
+                        .is_none()
+                    {
+                        panic!("multibody_joints should be able to get all links returned from rigid_body_link.");
+                    }
+                    panic!("This test has only 1 link, it should lead to rigid_body_link returning `None` immediately after first removal.");
+                }
             }
             pipeline.step(
                 &Vector::zeros(),
@@ -76,61 +113,6 @@ mod test {
                 &(),
                 &(),
             );
-            match k {
-                0 => {} // Remove in insertion order.
-                1 => {
-                    // Remove from leaf to root.
-                    handles.reverse();
-                }
-                _ => {
-                    // Shuffle the vector a bit.
-                    // (This test checks multiple shuffle arrangements due to k > 2).
-                    for l in 0..num_links {
-                        handles.swap(l, rnd.rand_range(0..num_links as u32) as usize);
-                    }
-                }
-            }
-
-            for handle in handles.clone().iter() {
-                bodies.remove(
-                    *handle,
-                    &mut islands,
-                    &mut colliders,
-                    &mut impulse_joints,
-                    &mut multibody_joints,
-                    true,
-                );
-                // Remove from our handles the one we just removed.
-                handles.retain(|value| value != handle);
-                // All handles left should be correctly set up.
-                for handle in handles.iter() {
-                    // Note debug #847: rigid_body_link should return none when attempting to remove the last (second) body.
-                    if let Some(link) = multibody_joints.rigid_body_link(*handle).copied() {
-                        if multibody_joints
-                            .get_multibody_mut_internal(link.multibody)
-                            .is_none()
-                        {
-                            panic!("multibody_joints should be able to get all links returned from rigid_body_link.");
-                        }
-                        panic!("This test has only 1 link, it should lead to rigid_body_link returning `None` immediately after first removal.");
-                    }
-                }
-                pipeline.step(
-                    &Vector::zeros(),
-                    &IntegrationParameters::default(),
-                    &mut islands,
-                    &mut bf,
-                    &mut nf,
-                    &mut bodies,
-                    &mut colliders,
-                    &mut impulse_joints,
-                    &mut multibody_joints,
-                    &mut CCDSolver::new(),
-                    None,
-                    &(),
-                    &(),
-                );
-            }
         }
     }
 }
