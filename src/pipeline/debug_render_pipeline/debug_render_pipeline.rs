0391465bbb6fe3b02d4e7c06d970f660272c36e7
Styly <claudiotorresptpt+git@gmail.com>
2026-09-08 01:16:47 +0100

    Fix convex hull debug rendering of internal edges

--- src/pipeline/debug_render_pipeline/debug_render_pipeline.rs
+++ src/pipeline/debug_render_pipeline/debug_render_pipeline.rs
@@ -584,6 +584,8 @@ impl DebugRenderPipeline {
                 let indices: Vec<_> = s
                     .edges()
                     .iter()
+                    // Coplanar triangles share a face after merging; skip their internal edges.
+                    .filter(|e| e.faces[0] != e.faces[1])
                     .map(|e| [e.vertices[0], e.vertices[1]])
                     .collect();
                 backend.draw_polyline(object, s.points(), &indices, pos, Vector::splat(1.0), color)
